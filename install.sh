17a08130c03799ad60068121fec4513048bb08e3
John-Ke <John-Ke@users.noreply.github.com>
2024-05-15 13:45:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,50 @@
 #!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  printf "${GREEN}";
+printf "'########::'##:::'##:'########:'########:\n";
+printf " ##.... ##:. ##:'##::... ##..:: ##.....::\n";
+printf " ##:::: ##::. ####:::::: ##:::: ##:::::::\n";
+printf " ########::::. ##::::::: ##:::: ######:::\n";
+printf " ##.... ##:::: ##::::::: ##:::: ##...::::\n";
+printf " ##:::: ##:::: ##::::::: ##:::: ##:::::::\n";
+printf " ########::::: ##::::::: ##:::: ########:\n";
+printf ".........::::::..::::::::..:::::........:\n";
+  printf "\n";
+  printf "ESSE MATERIAL FAZ PARTE DA BYTEHOST\n";
+  printf "\n";
+  printf "Compartilhar, vender ou fornecer essa solução\n";
+  printf "sem autorização é crime previsto no artigo 184\n";
+  printf "do código penal que descreve a conduta criminosa\n";
+  printf "de infringir os direitos autorais da BYTEHOST.\n";
+  printf "\n";
+  printf "PIRATEAR ESSA SOLUÇÃO É CRIME.\n";
+  printf "\n";
+  printf " © Bytehost.com.br\n";
+  printf "${NC}";
+
+  printf "\n"
+}
 
 # Verifica se o usuário é root
 if [ "$(id -u)" -ne 0 ]; then
