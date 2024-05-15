f48f6cf88818c93e841b9c168723905ce25f4064
John-Ke <John-Ke@users.noreply.github.com>
2024-05-15 16:28:40 -0400

    Update install.sh
    log metabase

--- install.sh
+++ install.sh
@@ -110,7 +110,6 @@ system_dependencies
 
 system_java
 system_java() {
-# install java
   print_banner
   printf "${WHITE} 💻 Instalando Java JDK...${GRAY_LIGHT}"
   printf "\n\n"
@@ -119,6 +118,7 @@ system_java() {
 
   sudo su - root <<EOF
   sudo apt install openjdk-11-jdk openjdk-11-jre
+
 EOF
 
   sleep 2
@@ -127,7 +127,6 @@ system_java
 
 system_db
 system_db() {
-# install java
   print_banner
   printf "${WHITE} 💻 Instalando Banco de Dados...${GRAY_LIGHT}"
   printf "\n\n"
@@ -137,7 +136,6 @@ system_db() {
   sudo su - root <<EOF
   sudo apt-get install mysql-server mysql-client -y
 EOF
-
   sleep 2
 }
 system_db
@@ -165,7 +163,6 @@ system_db
 
 metabase_download
 metabase_download() {
-# install java
   print_banner
   printf "${WHITE} 💻 Donwload do Metabase...${GRAY_LIGHT}"
   printf "\n\n"
@@ -175,16 +172,57 @@ metabase_download() {
   sudo su - root <<EOF
   sudo mkdir /home/metabase
   cd /home/metabase
-  sudo wget https://downloads.metabase.com/v0.49.10/metabase.jar
+#  sudo wget https://downloads.metabase.com/v0.49.10/metabase.jar
   
 EOF
-
   sleep 2
 }
 metabase_download
 
+metabase_config
+metabase_config() {
+  print_banner
+  printf "${WHITE} 💻 Ajustando o ambiente para o Metabase...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  cd /home/metabase
+  sudo groupadd -r metabase
+  sudo useradd -r -s /bin/false -g metabase metabase
+  sudo chown -R metabase:metabase /home/metabase
+  sudo touch /var/log/metabase.log
+  sudo chown metabase:metabase /var/log/metabase.log
+  sudo touch /etc/default/metabase
+  sudo chmod 775 /etc/default/metabase
+  
+  
+EOF
+  sleep 2
+}
+metabase_config
+
+metabase_config_log
+metabase_config() {
+  print_banner
+  printf "${WHITE} 💻 Ajustando logs do Metabase...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  cd /etc/rsyslog.d/
+  cat > metabase.conf << 'END'
+  if $programname == 'metabase' then /var/log/metabase.log
+  & stop
+END
+    sudo chmod 775 /var/log/metabase.log
+EOF
+  sleep 2
+}
+metabase_config_log
+
 
 ## bash <(curl -sSL setup.bytehost.com.br)
-
-
-# https://downloads.metabase.com/v0.49.10/metabase.jar
+## https://downloads.metabase.com/v0.49.10/metabase.jar
