c353300bd462c508a7365082f5a856d459e73ec1
John-Ke <John-Ke@users.noreply.github.com>
2024-05-15 11:47:51 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Verifica se o usuário é root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Este script precisa ser executado como root. Executando sudo su..."
+    sudo su
+fi
+
+# Verifica se o usuário está no diretório /root/
+if [ "$PWD" != "/root" ]; then
+    echo "Mudando para o diretório /root/"
+    cd /root || exit
+fi
+
+# Verifica se está usando Ubuntu 20.04
+if ! grep -q 'Ubuntu 20.04' /etc/os-release; then
+    echo "Este script recomenda o uso do Ubuntu 20.04."
+    sleep 3
+fi
+
+# update and install dependencies required by metabase
+sudo apt-get install -y apt-utils
+
+sudo apt update
+
+sudo apt upgrade -y
+
+# install java
+sudo apt install openjdk-11-jdk openjdk-11-jre
+
+# add user & group
